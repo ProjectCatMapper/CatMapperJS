62196e54639c83838d1e25b4f610aef343a8ba01
bischrob <bischrob@gmail.com>
2023-11-02 12:40:58 -0700

    adding more changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { Neo4jProvider, createDriver } from 'use-neo4j'// Create driver instance
+const driver = createDriver('neo4j', 'localhost', 7687, 'neo4j', '[REDACTED]')
+/*
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const client = new ApolloClient({
+
+  uri: 'sociomap.rc.asu.edu:7474',
+
+  cache: new InMemoryCache(),
+
+});
+*/
+ReactDOM.render(
+  <BrowserRouter>
+      <Auth0Provider
+    domain="dev-onnldmkcx3jun1em.us.auth0.com"
+    clientId="2QWgZvzukTh348ToXR7RbvgiaulSmSfc"
+    authorizationParams={{
+      redirect_uri: 'http://localhost:3000/Profile/'
+    }}
+  >
+      <Neo4jProvider driver={driver}>
+      <App />
+    </Neo4jProvider>
+  </Auth0Provider>
+   </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
