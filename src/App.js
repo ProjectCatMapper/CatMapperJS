4bb3045ab02d377693452d69d32df486b9beb9ce
bischrob <bischrob@gmail.com>
2023-11-02 12:40:58 -0700

    adding more changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+
+import Home from './routes/Home'
+import Explore from './routes/Explore'
+import Catmapper from "./routes/Catmapper";
+import Archamap from "./routes/Archamap"
+import Profile from "./routes/Profile";
+import Sociomapclick from "./routes/Sociomapclick";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path='/' element={<Catmapper />} />
+        <Route path='/Archamap' element={<Archamap />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/explore' element={<Explore />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/exview/:socioid' element={<Sociomapclick />} />
+      </Routes>
   );
 }
 
