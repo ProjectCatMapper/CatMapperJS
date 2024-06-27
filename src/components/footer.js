00707db9b6917e985409cbd128c7a3555716417a
Harsha-Kasi <harshakasijp7@gmail.com>
2024-06-27 08:11:04 -0700

    foci table

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect,useState } from 'react';
 // import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -29,11 +30,16 @@ import Divider from '@mui/material/Divider';
 //     color: '#fff',
 //   }));
 
+
   function createData(nodes, relations, DatasetProgress ) {
     return { nodes, relations, DatasetProgress };
   }
 
-  const rows = [
+  function createFoci(Focus, Datasets, Areas, Ethnicities, Languages, Religions) {
+    return { Focus, Datasets, Areas, Ethnicities, Languages, Religions  };
+  }
+
+    const rows = [
     createData('2,194 DATASETS','258,573 CONTAINS','DISTRICTS 310,839'),
     createData('205,536 DISTRICTS ','253,386 DISTRICT OF','ETHNICITIES 127,553'),
     createData('11,955 ETHNICITIES ','','GENERICS 386'),
@@ -42,7 +48,7 @@ import Divider from '@mui/material/Divider';
     createData('2,561 RELIGIONS','','VARIABLES	17,108'),
     createData('104	VARIABLES','524,517	ENCODINGS',''),
   ];
-  
+ 
   const handleButtonClick = async () => {
     try {
       const response = await fetch('https://catmapper.org/api/allDatasets?database=sociomap', {
@@ -72,7 +78,38 @@ import Divider from '@mui/material/Divider';
     }
   };
 
-const footer = () => {
+const Footer = () => {
+
+  const [foci,setfoci] = useState([])
+
+  useEffect(() => {
+    fetch("https:// catmapper.org/api/foci?database=sociomap",
+        // fetch("http://127.0.0.1:5001/foci?database=sociomap",
+            {
+                method: "GET"
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {setfoci([
+              createFoci(data[0].Focus,data[0].Datasets,data[0].AREAS,data[0].ETHNICITIES,data[0]["LANGUAGES, DIALECTS, and FAMILIES"],data[0].RELIGIONS),
+              createFoci(data[1].Focus,data[1].Datasets,data[1].AREAS,data[1].ETHNICITIES,data[1]["LANGUAGES, DIALECTS, and FAMILIES"],data[1].RELIGIONS),
+              createFoci(data[2].Focus,data[2].Datasets,data[2].AREAS,data[2].ETHNICITIES,data[2]["LANGUAGES, DIALECTS, and FAMILIES"],data[2].RELIGIONS),
+              createFoci(data[3].Focus,data[3].Datasets,data[3].AREAS,data[3].ETHNICITIES,data[3]["LANGUAGES, DIALECTS, and FAMILIES"],data[3].RELIGIONS),
+              createFoci(data[4].Focus,data[4].Datasets,data[4].AREAS,data[4].ETHNICITIES,data[4]["LANGUAGES, DIALECTS, and FAMILIES"],data[4].RELIGIONS),
+            ]);
+            })
+    },[])
+
+    // const fociRows = [
+    //   createFoci(foci[0].Focus,foci[0].Datasets,foci[0].AREAS,foci[0].ETHNICITIES,foci[0]["LANGUAGES, DIALECTS, and FAMILIES"],foci[0].RELIGIONS),
+    //   createFoci(foci[1].Focus,foci[1].Datasets,foci[1].AREAS,foci[1].ETHNICITIES,foci[1]["LANGUAGES, DIALECTS, and FAMILIES"],foci[1].RELIGIONS),
+    //   createFoci(foci[2].Focus,foci[2].Datasets,foci[2].AREAS,foci[2].ETHNICITIES,foci[2]["LANGUAGES, DIALECTS, and FAMILIES"],foci[2].RELIGIONS),
+    //   createFoci(foci[3].Focus,foci[3].Datasets,foci[3].AREAS,foci[3].ETHNICITIES,foci[3]["LANGUAGES, DIALECTS, and FAMILIES"],foci[3].RELIGIONS),
+    //   createFoci(foci[4].Focus,foci[4].Datasets,foci[4].AREAS,foci[4].ETHNICITIES,foci[4]["LANGUAGES, DIALECTS, and FAMILIES"],foci[4].RELIGIONS),
+    // ];
+  
+  
   return (
     <div className='footer'>
 
@@ -148,6 +185,48 @@ Download datasets list      </Button>
     </Card>
       </Grid>
 
+      <Grid item xs={6}>
+      <Card variant="outlined" style={{backgroundColor: 'black',border: '1px solid white',}}>
+        <CardContent>
+      <Typography id='sociomapfooter' sx={{ fontSize: 20 }} color="#fff" gutterBottom>Foci Status</Typography>
+      <Typography variant="table" color="#000" component="div">
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="justify">Focus</TableCell>
+            <TableCell align="justify">Datasets</TableCell>
+            <TableCell align="justify">Areas</TableCell>
+            <TableCell align="justify">Ethnicities</TableCell>
+            <TableCell align="justify">Languages</TableCell>
+            <TableCell align="justify">Religions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {foci.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.Focus}
+              </TableCell>
+              <TableCell align="left">{row.Datasets}</TableCell>
+              <TableCell align="left">{row.Areas}</TableCell>
+              <TableCell align="left">{row.Ethnicities}</TableCell>
+              <TableCell align="left">{row.Languages}</TableCell>
+              <TableCell align="left">{row.Religions}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+      </Typography>
+    </CardContent>
+    </Card>
+      </Grid>
+
       {/* <Grid item xs={6}>
       <Card variant="outlined" style={{backgroundColor: 'black',border: '1px solid white',}}>
         <CardContent>
@@ -204,4 +283,4 @@ Download datasets list      </Button>
   )
 }
 
-export default footer
+export default Footer
