924b389728ff54b4c3db64d0eca5a2fea748c39f
Harsha-Kasi <harshakasijp7@gmail.com>
2024-10-03 12:45:07 -0700

    api  change

--- src/components/uploadtranslate.js
+++ src/components/uploadtranslate.js
@@ -87,8 +87,8 @@ ExcelRenderer(fileObj, (err, resp) => {
 
   const handleSubmit = async () => {
     try {
-      // const response = await fetch("https://catmapper.org/api/uploadInputNodes",{
-        const response = await fetch("http://127.0.0.1:5001/uploadInputNodes", {
+      const response = await fetch("https://catmapper.org/api/uploadInputNodes",{
+        //const response = await fetch("http://127.0.0.1:5001/uploadInputNodes", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
